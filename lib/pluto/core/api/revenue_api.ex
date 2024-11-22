69f9fb4c0f502cd3de240e104913bc5da9398bb3
AlvesMatheusO <matheusalv.oliveira@gmail.com>
2024-11-22 17:47:14 -0300

    create, delete

--- lib/pluto/core/api/revenue_api.ex
+++ lib/pluto/core/api/revenue_api.ex
@@ -4,11 +4,41 @@ defmodule Pluto.Core.Revenue.Api do
   alias Pluto.Repo
   alias Pluto.Core.Revenue
 
+  import Ecto.Query
+
   # get All revenues
   def all do
     Repo.all(Revenue)
   end
 
+  # get ID
+  def get(id) do
+    Repo.get(Revenue, id)
+  end
+
+  # get by
+  def get_by(where) do
+    from(Revenue)
+    |> where(^where)
+    |>  Repo.one()
+  end
+
+  # create revenue
+  def insert(params) do
+    struct(Revenue, params)
+    |> Repo.insert()
+  end
+
+  # delete revenue
+  def delete(id) do
+    case get(id) do
+      nil -> nil
+      revenue -> Repo.delete(revenue)
+    end
+
+  end
+
+  # edit revenue
 
 
 end
